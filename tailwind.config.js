5b01a4988ed988a1f5b2df1e788eab4d7e25df30
md somrat islam <islammdsomrat05@gmail.com>
2024-11-06 12:15:44 +0600

    Tailwnd css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    colors: {},
+    colors: {
+      "primary-color": "#4BAf47",
+    },
+  },
+  plugins: [],
+};
